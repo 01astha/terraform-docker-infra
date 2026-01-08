69f936122a8cd8724d346f78924d21f53ff54260
Astha Singh <aastha9198singh@gmail.com>
2026-01-09 01:48:17 +0530

    Fix: upgrade docker provider to avoid expired signing key

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_providers {
-        docker = {
-            source = "kreuzwerker/docker"
-            version = "~> 3.0"
-        }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
     }
-}
+  }
 
-provider "docker" {}
+  required_version = ">= 1.3.0"
+}
